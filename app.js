d4cac91739c17c7e9fc23f29edd266b7a247a21a
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-10-16 19:57:28 +0300

    Added persistent data storage with mongodb. Support for adding posts. Dynamically displaying posts

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const path = require("path")
 const ejs = require("ejs")
+const Post = require("./models/Post");
 
 const app = express();
 
-app.set("view engine", "ejs")
-app.use(express.static("public"))
-
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.render("index");
+//Set up default mongoose connection
+var mongoDB = 'mongodb://localhost/cleanblog-test-db';
+mongoose.connect(mongoDB, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+});
+
+// TEMPLATE ENGINE
+app.set("view engine", "ejs")
+
+// MIDDLEWARES
+app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  console.log(`posts`, posts)
+  res.render("index", {
+    posts
+  });
 });
 
 app.get('/about', (req, res) => {
   res.render("about");
 });
+
 app.get('/post', (req, res) => {
   res.render("post");
 });
+
 app.get('/add_post', (req, res) => {
   res.render("add_post");
 });
+
+app.post("/posts", async (req,res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}.`);
